e5962240b626bbdad380246a4fef4f9b2f4daba1
asalih123-sys <ame2164160@maricopa.edu>
2026-08-11 09:23:03 -0700

    Added more styling fixed menu pages just needs information continuing to add more context needs more information to be added to the menu pages and more styling to be done to the menu pages

---
+++ pages/products/products.js
@@ -0,0 +1,160 @@
+let breakfast = [
+    {
+        name:"Quantum Pancakes",
+        price:8.50,
+        featured:true 
+    },
+
+    {
+        name:"Pixel Waffles",
+        price:9.00,
+        featured:false
+    },
+
+    {
+        name:"Cyber Omelet",
+        price:14.71,
+        featured:false
+    },
+
+    {
+        name:"Neon Breakfast Bowl",
+        price:7.50,
+        featured:false
+    },
+
+    {
+        name:"Circuit French Toast",
+        price:11.50,
+        featured:true
+    },
+]
+
+let lunch = [
+    {
+        name:"Glitch Burger",
+        price:14.00,
+        featured:false
+    },
+
+    {
+        name:"Al Chicken Sandwich",
+        price:14.00,
+        featured:false
+    },
+
+    {
+        name:"Quantum Wrap",
+        price:9.44,
+        featured:false
+    },
+
+    {
+        name:"Fusion Ramen Bowl",
+        price:21.14,
+        featured:true
+    },
+
+    {
+        name:"Hologram Club Sandwich",
+        price:14.00,
+        featured:false
+    },
+]
+
+let dinner = [
+    {
+        name:"Plasma Steak",
+        price:33.80,
+        featured:true
+    },
+
+    {
+        name:"Binary Salmon",
+        price:25.30,
+        featured:false
+    },
+
+    {
+        name:"Quantum Noodle Bowl",
+        price:21.14,
+        featured:"true"
+    },
+
+    {
+        name:"Neon Teriyaki Chicken",
+        price:15.16,
+        featured:false
+    },
+
+    {
+        name:"Cyber Rice Plate",
+        price:14.00,
+        featured:false
+    },
+]
+
+let dessert = [
+   {
+        name:"Data Cube Cheesecake",
+        price:5.99,
+        featured:false
+    },
+
+    {
+        name:"Pixel Ice Cream",
+        price:5.99,
+        featured:false
+    },
+
+    {
+        name:"Neon Cheesecake",
+        price:5.99,
+        featured:false
+    },
+
+    {
+        name:"Quantum Cookie Sundae",
+        price:4.50,
+        featured:false
+    },
+
+    {
+        name:"Hologram Brownie",
+        price:4.50,
+        featured:false
+    },
+]
+
+
+let drinks = [
+    {
+        name:"Blue Screen Lemonade",
+        price:2.99,
+        featured:false
+    },
+
+    {
+        name:"RGB Energy Soda",
+        price:2.99,
+        featured:"false"
+    },
+
+    {
+        name:"Liquid Lighting",
+        price:2.99,
+        featured:"false"
+    },
+
+    {
+        name:"Nano Tea",
+        price:2.99,
+        featured: false
+    },
+
+    {
+        name:"Quantum Shake",
+        price:2.99,
+        featured:false
+    },
+]
