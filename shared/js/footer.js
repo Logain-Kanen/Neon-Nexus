a29eba7ec284efde9bee3ad7a96fa0a4cbb1115e
Logain-Kanen <lechantler2008@gmail.com>
2026-08-18 09:11:34 -0700

    Theme Spread to menus, exact colors/details can be changed

--- shared/js/footer.js
+++ shared/js/footer.js
@@ -4,22 +4,30 @@ function setTheme() {
   if (theme == "light") {
     document.querySelector("main").classList.add("light-main");
     document.querySelector("body").classList.add("light-body");
-    document.querySelector(".slogan").classList.add("light-slogan");
+    document.querySelector(".main-h1").classList.add("light-slogan");
+    document.querySelector(".popup-div").classList.add("light-popup");
     document.querySelectorAll(".main-div").forEach((e) => {
       e.classList.add("light-div");
     });
     document.querySelectorAll(".main-h2").forEach((e) => {
       e.classList.add("light-h2");
     });
+    document.querySelectorAll(".cart-btn").forEach((e) => {
+      e.classList.add("light-btn");
+    });
   } else if (theme == "dark") {
     document.querySelector("main").classList.remove("light-main");
     document.querySelector("body").classList.remove("light-body");
-    document.querySelector(".slogan").classList.remove("light-slogan");
+    document.querySelector(".main-h1").classList.remove("light-slogan");
+    document.querySelector(".popup-div").classList.remove("light-popup");
     document.querySelectorAll(".main-div").forEach((e) => {
       e.classList.remove("light-div");
     });
     document.querySelectorAll(".main-h2").forEach((e) => {
       e.classList.remove("light-h2");
+    });
+    document.querySelectorAll(".cart-btn").forEach((e) => {
+      e.classList.remove("light-btn");
     });
   }
 }
--- shared/js/global.js
+++ shared/js/global.js
@@ -1,4 +1,4 @@
-let popupDiv = document.getElementById("popup-div");
+let popupDiv = document.querySelector(".popup-div");
 function openCart() {
   popupDiv.style.display = "flex";
 }
