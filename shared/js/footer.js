0f2115b0508c0a0713f6da213885a009250ce566
Logain-Kanen <lechantler2008@gmail.com>
2026-08-18 08:39:58 -0700

    Theme-Switch for Homepage, Need to spread to other pages

--- shared/js/footer.js
+++ shared/js/footer.js
@@ -1,10 +1,37 @@
-let theme;
-localStorage.setItem("theme", "dark");
+let theme = "";
+theme = localStorage.getItem("theme");
+function setTheme() {
+  if (theme == "light") {
+    document.querySelector("main").classList.add("light-main");
+    document.querySelector("body").classList.add("light-body");
+    document.querySelector(".slogan").classList.add("light-slogan");
+    document.querySelectorAll(".main-div").forEach((e) => {
+      e.classList.add("light-div");
+    });
+    document.querySelectorAll(".main-h2").forEach((e) => {
+      e.classList.add("light-h2");
+    });
+  } else if (theme == "dark") {
+    document.querySelector("main").classList.remove("light-main");
+    document.querySelector("body").classList.remove("light-body");
+    document.querySelector(".slogan").classList.remove("light-slogan");
+    document.querySelectorAll(".main-div").forEach((e) => {
+      e.classList.remove("light-div");
+    });
+    document.querySelectorAll(".main-h2").forEach((e) => {
+      e.classList.remove("light-h2");
+    });
+  }
+}
 
 function themeSwitch() {
-  theme = localStorage.getItem("theme");
-  if ((theme = "dark")) {
-    document.querySelector("main").classList.add("light-main");
-    document.querySelector("body").classList.add("light-body")
+  if (theme == "dark" || theme == null) {
+    localStorage.setItem("theme", "light");
+    theme = localStorage.getItem("theme");
+    setTheme();
+  } else if (theme == "light") {
+    localStorage.setItem("theme", "dark");
+    theme = localStorage.getItem("theme");
+    setTheme();
   }
 }
